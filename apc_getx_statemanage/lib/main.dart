f78404c725716f144bf4c946b3680005af998aff
robbinl <robbinl@telusplanet.net>
2021-02-10 15:09:55 -0700

    a

---
+++ apc_getx_statemanage/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:robbinlaw/screens/first.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      //don't need 'Get' before Material App if you're only using State Management
+      home: First(),
+      theme: ThemeData(
+        accentColor: Colors.purpleAccent,
+        primaryColor: Colors.purple,
+      ),
+    ),
+  );
+}
