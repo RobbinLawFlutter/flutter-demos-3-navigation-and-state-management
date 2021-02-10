a083cfee85685b631d135690654e29c6072ec0d8
robbinl <robbinl@telusplanet.net>
2021-02-10 16:59:44 -0700

    a

--- ll-state-management-getx/lib/controllers/countController.dart
+++ ll-state-management-getx/lib/controllers/countController.dart
@@ -2,9 +2,9 @@ import 'package:get/state_manager.dart';
 import 'package:robbinlaw/models/count.dart';
 
 class CountController extends GetxController {
-  Rx<CounterModel> _counterModelStream = Rx<CounterModel>();
-  int get count => _counterModelStream.value.count;
+  CounterModel _counterModel = CounterModel();
+  int get count => _counterModel.count;
   void increment() {
-    _counterModelStream.value.increment();
+    _counterModel.increment();
   }
 }
