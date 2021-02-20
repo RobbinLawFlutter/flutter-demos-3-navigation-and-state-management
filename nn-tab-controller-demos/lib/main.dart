09f651b363a66d8a8683c8cb2a0995c741c4679b
robbinl <robbinl@telusplanet.net>
2021-02-20 14:51:51 -0700

    a

--- nn-tab-controller-demos/lib/main.dart
+++ nn-tab-controller-demos/lib/main.dart
@@ -7,6 +7,14 @@ import 'package:get/get.dart';
 import 'package:robbinlaw/bindings/globalBindings.dart';
 import 'package:robbinlaw/views/home.dart';
 
+import 'package:robbinlaw/demos/ac_tabbed_page_demo_1.dart';
+import 'package:robbinlaw/demos/ae_tabbed_page_demo_2.dart';
+import 'package:robbinlaw/demos/ag_tabbed_page_demo_3.dart';
+import 'package:robbinlaw/demos/aj_scoped_model_demo_1.dart';
+import 'package:robbinlaw/demos/ak_scoped_model_demo_2.dart';
+import 'package:robbinlaw/demos/ay_AlertDialog_SnackBar_Demo.dart';
+import 'package:robbinlaw/demos/am_tabbed_page_scoped_model_demo.dart';
+
 //don't need 'Get' before Material App if you're only using State Management
 //but we will also use Get for Navigation Management so we will need it.
 void main() {
@@ -20,7 +28,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //dependancy injection via Bindings().
       initialBinding: GlobalBindings(),
-      home: HomeView(),
+      //home: HomeView(),
+      home: MyTabbedDemo1(),
+      // home: MyTabbedDemo2(),
+      // home: MyTabbedDemo3(),
+      // home: MyScopedModelDemo1(
+      //   myModel: MyCounterModel(),
+      //),
+
+      // home: MyScopedModelDemo2(),
+      // home: MyTabbedScopedModelDemo(),
+      //home: AlertDialogAndSnackBarDemo(),
       theme: ThemeData.dark(),
     );
   }
