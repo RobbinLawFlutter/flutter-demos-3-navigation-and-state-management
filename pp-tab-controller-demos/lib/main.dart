c48898b87b4c0713f690538b261e4d92ca314d81
robbinl <robbinl@telusplanet.net>
2021-02-27 12:15:31 -0700

    a

--- pp-tab-controller-demos/lib/main.dart
+++ pp-tab-controller-demos/lib/main.dart
@@ -1,11 +1,6 @@
-//getx (navigation manager and state manager) documentation from medium
-//https://medium.com/flutter-community/the-flutter-getx-ecosystem-dependency-injection-8e763d0ec6b9
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:robbinlaw/bindings/globalBindings.dart';
-import 'package:robbinlaw/views/home.dart';
 
 import 'package:robbinlaw/demos/aa-tab-demo-1.dart';
 import 'package:robbinlaw/demos/cc-tab-demo-2.dart';
@@ -21,13 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      //dependancy injection via Bindings().
-      initialBinding: GlobalBindings(),
       //home: MyTabbedDemo1(),
       //home: MyTabbedDemo2(),
       //home: MyTabbedDemo3(),
       //home: AlertDialogAndSnackBarDemo(),
-      home: HomeView(),
       theme: ThemeData.dark(),
     );
   }
--- pp-tab-controller-demos/lib/models/numberModel.dart
+++
@@ -1,7 +0,0 @@
-class NumberModel {
-  //Constructor
-  NumberModel({this.title, this.image});
-  //Public fields.
-  final String title;
-  final String image;
-}
