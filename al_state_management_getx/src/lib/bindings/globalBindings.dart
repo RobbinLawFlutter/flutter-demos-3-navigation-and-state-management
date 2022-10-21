ff7a92e3ee640b6ccebb32ddbbba33bb44828922
robbinl <robbinl@telusplanet.net>
2022-10-21 09:31:14 -0600

    a

--- al_state_management_getx/src/lib/bindings/globalBindings.dart
+++ al_state_management_getx/src/lib/bindings/globalBindings.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:get/get.dart';
 import 'package:robbinlaw/controllers/countController.dart';
 import 'package:robbinlaw/controllers/userController.dart';
